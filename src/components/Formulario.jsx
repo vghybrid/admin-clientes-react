5f5e5d8a67d2ece4507ecf8111cf0327d1bdb31f
vghybrid <anderson.sarmiento13@gmail.com>
2022-06-20 12:24:07 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Formik, Form, Field } from 'formik'
+
+const Formulario = () => {
+  return (
+    <div className='md:w-3/4 mx-auto mt-10 px-5 py-10 rounded-md shadow-md bg-white '>
+        <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar Cliente</h1>
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/paginas/EditarCliente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditarCliente = () => {
+  return (
+    <div>
+        <h1>Editar Ciente</h1>
+    </div>
+  )
+}
+
+export default EditarCliente
