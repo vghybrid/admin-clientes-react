e91a4ec8e359dafe205f163cef43ac4c3bebc8e0
vghybrid <anderson.sarmiento13@gmail.com>
2022-06-20 12:26:16 -0300

    Primer dia

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,6 +5,11 @@ const Formulario = () => {
   return (
     <div className='md:w-3/4 mx-auto mt-10 px-5 py-10 rounded-md shadow-md bg-white '>
         <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar Cliente</h1>
+
+        <Formik>
+
+            
+        </Formik>
     </div>
   )
 }
