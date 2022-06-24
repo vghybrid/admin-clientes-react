60918d8a814514b53232bbbbe1b29ad71f9369f7
vghybrid <anderson.sarmiento13@gmail.com>
2022-06-24 12:47:54 -0300

    Cuarto dia - deployment

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
   useEffect(() => {
       const obtenerClientesAPI = async () => {
         try {
-            const url = 'http://localhost:4000/clientes'
+            const url = import.meta.env.VITE_API_URL
             const respuesta = await fetch(url)
             const resultado = await respuesta.json()
             setClientes(resultado) 
@@ -23,7 +23,7 @@ const Inicio = () => {
     const confirmar = confirm('¿Realmente deseas eliminar este cliente de forma permanente?')
     if(confirmar) {
         try {
-            const url = `http://localhost:4000/clientes/${id}`
+            const url = `${import.meta.env.VITE_API_URL}/${id}`
             const respuesta = await fetch(url, {
               method: 'DELETE'
             })
